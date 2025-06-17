348061ed51f536d6af94205f05cc3fa7bee49f33
vinit rathore <vinitrathore1277@gmail.com>
2025-06-17 18:21:13 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // allows access from local network
+    port: 5173,       // you can change this port if needed
+  },
 })
